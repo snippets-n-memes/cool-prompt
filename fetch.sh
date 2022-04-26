7cb2cac33048bd2c22dcc2313a42958cbafd74d6
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-04-26 17:19:19 -0500

    fetch url

---
+++ fetch.sh
@@ -0,0 +1,2 @@
+url="https://api.github.com/repos/mbmcmullen27/raspberry-spi/actions/workflows/24269544"
+curl   -H "Accept: application/vnd.github.v3+json"  "https://api.github.com/repos/mbmcmullen27/raspberry-spi/actions/workflows/24269544/runs"
