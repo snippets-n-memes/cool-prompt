16f3d8a3eea94a4e9f096ec7e18039a92879e249
mbmcmullen27 <68041054+mbmcmullen27@users.noreply.github.com>
2022-05-11 15:42:21 -0400

    Bad stash
    Unexpected conflicts from `git stash pop`

--- bashrc-block.sh
+++ bashrc-block.sh
@@ -1,29 +1,14 @@
-<<<<<<< Updated upstream
-CYAN='\033[0;36m'
-BLUE='\033[0;34m'
-RED='\033[0;31m'
-BLACK='\033[0;30m'
-END='\e[m'
-=======
 alias CYAN="set-color '\033[0;36m'"
 alias BLUE="set-color '\033[0;34m'"
 alias RED="set-color '\033[0;31m'"
 alias GREEN="set-color '\033[0;32m'"
 alias BLACK="set-color '\033[0;30m'"
 alias END="set-color '\e[m'"
->>>>>>> Stashed changes
 
 function set-template() {
   PS1="$CYAN[\u: \W]$(__git_ps1 " ⇵ %s")\n$END$(wf-get name)->$(wf-get status):$(wf-get conclusion)\n$ "
 }
 
-<<<<<<< Updated upstream
-function status-map () {
-  case "$1" in 
-    success)
-
-    exit;;
-=======
 function set-color() {
   echo -e "$1"
 }
@@ -36,7 +21,6 @@ function conclusion-map () {
     failure)
       echo "$(RED)⬤$(END)"
       exit;;
->>>>>>> Stashed changes
   esac
 }
 
