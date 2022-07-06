1d21cdc81cab8918e1a0813a020d327e3d9c945b
mbmcmullen27 <mbmcmullen27@gmail.com>
2022-07-05 21:49:18 -0500

    store hand info in /tmp

--- blackjack-functions.sh
+++ blackjack-functions.sh
@@ -1,31 +1,46 @@
 function deal() {
-    CARDS=$(curl https://www.deckofcardsapi.com/api/deck/$(deck_id)/draw/?count=2 \
-           | jq -j '.cards | map(.code) | join(",")')
-    
-    curl https://www.deckofcardsapi.com/api/deck/$(deck_id)/pile/player/add/?cards=$CARDS
+    CARDS=$(draw 2 | jq -j '.cards | map(.code) | join(",")')
+    curl -s https://www.deckofcardsapi.com/api/deck/$(deck_id)/pile/player/add/?cards=$CARDS
+    echo $CARDS > /tmp/blackjack_hand
 }
 
 function hit() {
-    CARD=$(curl https://www.deckofcardsapi.com/api/deck/$(deck_id)/draw/?count=1 \
-            | jq .cards[0].code)
-
-    curl https://www.deckofcardsapi.com/api/deck/$(deck_id)/pile/player/add/?cards=$CARD
+    CARD=$(draw 1 | jq .cards[0].code)
+    curl -s https://www.deckofcardsapi.com/api/deck/$(deck_id)/pile/player/add/?cards=$CARD
+    echo $CARD >> /tmp/blackjack_hand
 }
 
-function stay() {
-
+function draw() {
+    curl -s https://www.deckofcardsapi.com/api/deck/$(deck_id)/draw/?count=$1 
 }
 
-function split() {
-
+function reset() {
+    rm /tmp/blackjack_hand
+    curl -s https://www.deckofcardsapi.com/api/deck/$(deck_id)/pile/player/return/
 }
+
+# function score() {
+
+# }
+
+# function stay() {
+
+# }
+
+# function split() {
+
+# }
 
 function shuffle() {    
-    curl https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1 \
-        | jq .deck_id /tmp/blackjack_deck.json \
+    curl -s https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1 \
+        | jq -r .deck_id \
         > /tmp/blackjack_deck_id
 }
 
 function deck_id() {
     cat /tmp/blackjack_deck_id
 }
+
+# function hand() {
+
+# }
